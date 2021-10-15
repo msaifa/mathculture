fa7fa93e0e5ed447850632060f862af2233bfdcc
Saif Alikhan <msaifa.07@gmail.com>
2021-10-15 22:27:32 +0700

    ganti template

--- A2.php
+++ A2.php
@@ -52,17 +52,20 @@
           margin-right:3.5%;
         }
         #wadahChekbox{
-          margin-left: 59.8%;
-          margin-top:20.5%;
+          margin-left: 54.3%;
+          margin-top: 18.3%;
+        }
+        #wadahChekbox div{
+          margin-top: 6.3%;
         }
     </style>
 </head>
 <body>
   <div id="wadahChekbox">
-    <br><a href="A<?= $soalno ?>.php?answer=A"><img src="<?= $jawabanKu == 'A' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5.75%" height="5.75%"/></a></br>
-    <br><a href="A<?= $soalno ?>.php?answer=B"><img src="<?= $jawabanKu == 'B' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5.75%" height="5.75%"/></a></br>
-    <p><a href="A<?= $soalno ?>.php?answer=C"><img src="<?= $jawabanKu == 'C' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5.75%" height="5.75%"/></a></p>
-    <p><a href="A<?= $soalno ?>.php?answer=D"><img src="<?= $jawabanKu == 'D' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5.75%" height="5.75%"/></a></p>
+    <div><a href="A<?= $soalno ?>.php?answer=A"><img src="<?= $jawabanKu == 'A' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5%" height="5%"/></a></div>
+    <div><a href="A<?= $soalno ?>.php?answer=B"><img src="<?= $jawabanKu == 'B' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5%" height="5%"/></a></div>
+    <div><a href="A<?= $soalno ?>.php?answer=C"><img src="<?= $jawabanKu == 'C' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5%" height="5%"/></a></div>
+    <div><a href="A<?= $soalno ?>.php?answer=D"><img src="<?= $jawabanKu == 'D' ? 'img/button_opsi2.png' : 'img/button_opsi.png'?>" width="5%" height="5%"/></a></div>
   </div>
   <div id="wadahButton">
     <a href="A<?= $soalno-1 ?>.php" id="btnBack"><img src="img/ssebelum.png"  width="200" height="50" border="0"/></a>
