7f849924471312a1a3c19742ae25b8f8d955230d
Saif Alikhan <msaifa.07@gmail.com>
2021-10-25 09:21:02 +0700

    ubah init ke settingan server

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("localhost", "root", "", "mathculture");
+  $conn = mysqli_connect("localhost", "cucx8292_mathculture_user", "_a7V=N!m&XGZ", "cucx8292_mathculture");
 
   echo '<meta name="viewport" content="width=1024">' ;
 
